ea5fc767697d7f63fe9f1a6bc12b217831a83f56
BoboMJZhou <zhou970610@csie.ndhu.edu.tw>
2018-05-28 04:28:59 +0800

    1

--- PaCircle.js
+++ PaCircle.js
@@ -1,11 +1,7 @@
-//DESKTOP
-
-
-
 // JavaScript source code
 var game = new Phaser.Game(405, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-var btn_change, btn_option;
+var btn_change, btn_option, btn_setting, btn_question, btn_cancel;
 var background;
 var backgroundMusic;
 var bombSound;
@@ -15,7 +11,6 @@ var balls;
 var result = 'begin';
 var selectedBall = [];
 var selectedBall2 = [];
-var distanceArray = [];
 var dis = 'dis';
 var score = 0;
 var BALL_SIZE = 27;
@@ -26,6 +21,7 @@ var map;
 var isKill = false;
 var statusBar;
 var status_sum = 0;
+var skill = false;
 
 function preload() {
 
@@ -35,15 +31,18 @@ function preload() {
     game.load.spritesheet('gball', 'assets/gball.png', BALL_SIZE, BALL_SIZE);//green
     game.load.spritesheet('bball', 'assets/bball.png', BALL_SIZE, BALL_SIZE);//blue
     game.load.spritesheet('pball', 'assets/pball.png', BALL_SIZE, BALL_SIZE);//purple
-    game.load.image('sky', 'assets/skies/sunset.png');
+
+    game.load.spritesheet('btn_setting', 'assets/buttons/setting.png', 200, 200);
+    game.load.spritesheet('btn_option', 'assets/buttons/sound.png', 200, 200);
+    game.load.spritesheet('btn_question', 'assets/buttons/info.png', 200, 200);
+    game.load.spritesheet('btn_cancel', 'assets/buttons/cancel.png', 200, 200);
+    game.load.spritesheet('btn_change', 'assets/buttons/button_sprite_sheet.png', 193, 71);
 
     game.load.image('statBar', 'assets/statusbar.png');
-   
-    game.load.spritesheet('btn_change', 'assets/buttons/button_sprite_sheet.png', 193, 71);
-    game.load.spritesheet('btn_option', 'assets/buttons/plus.png', 200, 200);
+
     game.load.image('background', 'assets/misc/starfield.jpg');
-    game.load.audio('backgroundMusic', ['assets/music.mp3', 'assets/music.ogg']);
-    game.load.audio('bombSound', ['assets/BlowBubble.mp3', 'assets/BlowBubble.ogg']);
+    game.load.audio('backgroundMusic', ['assets/audio/music.mp3', 'assets/audio/music.ogg']);
+    game.load.audio('bombSound', ['assets/audio/BlowBubble.mp3', 'assets/audio/BlowBubble.ogg']);
     game.load.bitmapFont('desyrel', 'assets/fonts/bitmapFonts/desyrel.png', 'assets/fonts/bitmapFonts/desyrel.xml');
 
 }
@@ -56,13 +55,14 @@ function create() {
     //@Score texts
     //@music
     game.stage.backgroundColor = '#182d3b';
+    game.stage.disableVisibilityChange = true;
     background = game.add.tileSprite(0, 0, 405, 600, 'background');
     btn_option = game.add.button(0, 0, 'btn_option', onclickSound, this, 0);
     btn_change = game.add.button(game.world.centerX - 95, 405, 'btn_change', actionOnClick, this, 0);
     nameText = game.add.bitmapText(30, 150, 'desyrel', 'Pacircle', 100);
     scoreText = game.add.bitmapText(10, 10, 'desyrel', 'Score: ', 24);
-    backgroundMusic = game.add.audio('backgroundMusic', 1, true); 
-    bombSound = game.add.audio('bombSound', 1, true); 
+    backgroundMusic = game.add.audio('backgroundMusic', 1, true);
+    bombSound = game.add.audio('bombSound', 1, true);
     statusBar = new StatusBar();//status bar
     statusBar.scale.setTo(0.4, 0.4);
     statusBar.setPercent(status_sum);
@@ -91,13 +91,11 @@ function update() {
 function actionOnClick() {
 
     background.visible = !background.visible;
-    
     btn_change.destroy();
     nameText.destroy();
     game.time.events.repeat(Phaser.Timer.SECOND / 10, 100, createBall, this);
     setTimeout(function () {
         canClick = true;
-        //console.log(canClick);
         initPositionArray();
     }, needTime);
 }
@@ -106,22 +104,33 @@ function ballKilled() {
 
     if (selectedBall.length > 1) {
         setScore();
+        
         for (var i = 0; i < selectedBall.length ; i++) {
-            console.log("delete : " + " " + selectedBall[i].sprite.position + "\n" + selectedBall[i].sprite.key);
             selectedBall[i].sprite.destroy();
             bombSound.play();
             // Not sure if we need this : balls.remove(selectedBall[i]);
         }
-        status_sum += selectedBall.length;
-        statusBar.setPercent(status_sum);
-        //if (status_sum === 200) {
+        if (skill) {
+            status_sum = 0;
+            skill = !skill;
+        }
+        else {
+            status_sum += selectedBall.length*2;
+        }
+            statusBar.setPercent(status_sum);
+        if (status_sum >= 20) {
+            console.log(statusBar.bar);
+            statusBar.bar.tint = 0x00ff8e;
+            skill = true;
+        }
+        else
+            statusBar.bar.tint = 0xffffff;
 
-        //}
         selectedBall.splice(0, selectedBall.length);
         ballPosition.splice(0, ballPosition.length);
         ballPosition = createArray((game.height / BALL_SIZE) + 1, (game.width / BALL_SIZE) + 1);
-        isKill = true;
     }
+    isKill = true;
 }
 
 function ballName(n) {
@@ -149,10 +158,14 @@ function ballSelected(b) {
 
     if (selectedBall[0] !== b)
     {
+        for (var i = 0; i < selectedBall.length; i++)
+            selectedBall[i].sprite.tint = 0xffffff;
         selectedBall.splice(0, selectedBall.length);
-        if(selectedBall[0]===null)
-            cleanBall();
-        findColor(b);
+        if (selectedBall[0] === undefined) { cleanBall(); }
+        if (!skill)
+            findColor(b);
+        else
+            releaseSkill(b);
     }
     else
         ballKilled();
@@ -162,8 +175,8 @@ function cleanBall() {
 
     for (var i = 0; i < ballPosition.length; i++)
     {
-        for (varj = 0; j < ballPosition[i].length; j++) {
-            if (ballPosition[i][j].sprite.body.dirty === true)
+        for (var j = 0; j < ballPosition[i].length; j++) {
+            if (ballPosition[i][j]!==undefined)
                 ballPosition[i][j].sprite.body.dirty = false;
         }
     }
@@ -173,11 +186,17 @@ function click(pointer) {
 
     var bodie = game.physics.p2.hitTest(pointer.position);
     if (canClick && bodie.length !== 0) {
-        console.log(pointer.position + bodie[0].parent.sprite.key);
+        if (isKill)
+        {
+            canClick = false;
+            initPositionArray();
+            canClick = true;
+            isKill = false;
+        }
         ballSelected(bodie[0].parent);
     }
     else
-        console.log("CANNOT CLICK NOW");
+        console.log("CANNOT CLICK");
 }
 
 function createArray(col, row) {
@@ -201,75 +220,57 @@ function createBall() {
 
 function findColor(b)
 {
-
-    if (isKill) {
-        canClick = false;
-        console.log("init in find color");
-        initPositionArray();
-        sleep(1000);
-        canClick = true;
-        isKill = false;
-    }
     selectedBall.push(b);
     b.sprite.body.dirty = true;
+    b.sprite.tint = 0x000000;
 
     var brow = Math.round(b.sprite.world.x / BALL_SIZE);
     var bcol = Math.round((game.height - b.sprite.world.y) / BALL_SIZE);
-    console.log(b.sprite.world.x + " " + b.sprite.world.y + "\n" + brow + " " + bcol + " " + b.sprite.key);
-    console.log(ballPosition[brow][bcol].sprite.key);
     
-    if (isInside(brow - 1, bcol - 1)) {
+    if (isInside(brow - 1, bcol - 1) && Phaser.Math.distance(b.sprite.world.x, b.sprite.world.y, ballPosition[brow - 1][bcol - 1].sprite.world.x, ballPosition[brow - 1][bcol - 1].sprite.world.y) < 3 / 2 * BALL_SIZE) {
         if (ballPosition[brow - 1][bcol - 1].sprite.key === b.sprite.key && ballPosition[brow - 1][bcol - 1].sprite.body.dirty === false) {
-            console.log("--find around "  + brow - 1 + " " + bcol - 1 + " " + ballPosition[brow - 1][bcol - 1].sprite.key);
             findColor(ballPosition[brow - 1][bcol - 1]);
         }
         ballPosition[brow - 1][bcol - 1].sprite.body.dirty = true;
     }
-    if (isInside(brow - 1, bcol)){
+    if (isInside(brow - 1, bcol) && Phaser.Math.distance(b.sprite.world.x, b.sprite.world.y, ballPosition[brow - 1][bcol].sprite.world.x, ballPosition[brow - 1][bcol].sprite.world.y) < 3 / 2 * BALL_SIZE) {
         if(ballPosition[brow - 1][bcol].sprite.key === b.sprite.key && ballPosition[brow - 1][bcol].sprite.body.dirty === false) {
-            console.log("- find around " + brow - 1 + " " + bcol + " " + ballPosition[brow - 1][bcol].sprite.key);
             findColor(ballPosition[brow - 1][bcol]);
         }
         ballPosition[brow - 1][bcol].sprite.body.dirty = true;
     }
-    if (isInside(brow - 1, bcol + 1)) {
+    if (isInside(brow - 1, bcol + 1) && Phaser.Math.distance(b.sprite.world.x, b.sprite.world.y, ballPosition[brow - 1][bcol + 1].sprite.world.x, ballPosition[brow - 1][bcol + 1].sprite.world.y) < 3 / 2 * BALL_SIZE) {
         if (ballPosition[brow - 1][bcol + 1].sprite.key === b.sprite.key && ballPosition[brow - 1][bcol + 1].sprite.body.dirty === false) {
-            console.log("-+find around " + brow - 1 + " " + bcol + 1 + " " + ballPosition[brow - 1][bcol + 1].sprite.key);
             findColor(ballPosition[brow - 1][bcol + 1]);
         }
         ballPosition[brow - 1][bcol + 1].sprite.body.dirty = true;
     }
-    if (isInside(brow, bcol + 1)) {
+    if (isInside(brow, bcol + 1) && Phaser.Math.distance(b.sprite.world.x, b.sprite.world.y, ballPosition[brow][bcol + 1].sprite.world.x, ballPosition[brow][bcol + 1].sprite.world.y) < 3 / 2 * BALL_SIZE) {
         if (ballPosition[brow][bcol + 1].sprite.key === b.sprite.key && ballPosition[brow][bcol + 1].sprite.body.dirty === false) {
-            console.log(" +find around " + brow + " " + bcol + 1 + " " + ballPosition[brow][bcol + 1].sprite.key);
             findColor(ballPosition[brow][bcol + 1]);
         }
         ballPosition[brow][bcol + 1].sprite.body.dirty = true;
     }
-    if (isInside(brow + 1, bcol + 1)) {
+    if (isInside(brow + 1, bcol + 1) && Phaser.Math.distance(b.sprite.world.x, b.sprite.world.y, ballPosition[brow + 1][bcol + 1].sprite.world.x, ballPosition[brow + 1][bcol + 1].sprite.world.y) < 3 / 2 * BALL_SIZE) {
         if (ballPosition[brow + 1][bcol + 1].sprite.key === b.sprite.key && ballPosition[brow + 1][bcol + 1].sprite.body.dirty === false) {
-            console.log("++find around " + brow + 1 + " " + bcol + 1 + " " + ballPosition[brow + 1][bcol + 1].sprite.key);
             findColor(ballPosition[brow + 1][bcol + 1]);
         }
         ballPosition[brow + 1][bcol + 1].sprite.body.dirty = true;
     }
-    if (isInside(brow + 1, bcol)) {
+    if (isInside(brow + 1, bcol) && Phaser.Math.distance(b.sprite.world.x, b.sprite.world.y, ballPosition[brow + 1][bcol].sprite.world.x, ballPosition[brow + 1][bcol].sprite.world.y) < 3 / 2 * BALL_SIZE) {
         if (ballPosition[brow + 1][bcol].sprite.key === b.sprite.key && ballPosition[brow + 1][bcol].sprite.body.dirty === false) {
-            console.log("+ find around " + brow + 1 + " " + bcol + " " + ballPosition[brow + 1][bcol].sprite.key);
             findColor(ballPosition[brow + 1][bcol]);
         }
         ballPosition[brow + 1][bcol].sprite.body.dirty = true;
     }
-    if (isInside(brow + 1, bcol - 1)) {
+    if (isInside(brow + 1, bcol - 1) && Phaser.Math.distance(b.sprite.world.x, b.sprite.world.y, ballPosition[brow + 1][bcol - 1].sprite.world.x, ballPosition[brow + 1][bcol - 1].sprite.world.y) < 3 / 2 * BALL_SIZE) {
         if (ballPosition[brow + 1][bcol - 1].sprite.key === b.sprite.key && ballPosition[brow + 1][bcol - 1].sprite.body.dirty === false) {
-            console.log("+-find around " + brow + 1 + " " + bcol - 1 + " " + ballPosition[brow + 1][bcol - 1].sprite.key);
             findColor(ballPosition[brow + 1][bcol - 1]);
         }
         ballPosition[brow + 1][bcol - 1].sprite.body.dirty = true;
     }
-    if (isInside(brow, bcol - 1)) {
+    if (isInside(brow, bcol - 1) && Phaser.Math.distance(b.sprite.world.x, b.sprite.world.y, ballPosition[brow][bcol - 1].sprite.world.x, ballPosition[brow][bcol - 1].sprite.world.y) < 3 / 2 * BALL_SIZE) {
         if (ballPosition[brow][bcol - 1].sprite.key === b.sprite.key && ballPosition[brow][bcol - 1].sprite.body.dirty === false) {
-            console.log(" -find around " + brow + " " + bcol - 1 + " " + ballPosition[brow][bcol - 1].sprite.key);
             findColor(ballPosition[brow][bcol - 1]);
         }
         ballPosition[brow][bcol - 1].sprite.body.dirty = true;
@@ -284,8 +285,8 @@ function initPositionArray() {
     balls.forEach(function (ball) {
         var row = Math.floor(ball.world.x / BALL_SIZE);
         var col = Math.floor((game.height - ball.world.y) / BALL_SIZE);
+        ball.body.sprite.dirty = false;
         ballPosition[Math.round(ball.world.x / BALL_SIZE)][Math.round((game.height - ball.world.y) / BALL_SIZE)] = ball.body;
-        ballPosition[Math.round(ball.world.x / BALL_SIZE)][Math.round((game.height - ball.world.y) / BALL_SIZE)].sprite.body.dirty = false;
     }, this);
     console.log("init over!");
 
@@ -305,15 +306,49 @@ function onclickSound() {
     backgroundMusic.mute = !backgroundMusic.mute;
 }
 
+function releaseSkill(b){
+    
+    selectedBall.push(b)
+
+    b.sprite.body.dirty = true;
+    b.sprite.tint = 0x000000;
+
+    var bx=b.sprite.world.x;
+    var by=b.sprite.world.y;
+
+    var brow = Math.round(b.sprite.world.x / BALL_SIZE);
+    var bcol = Math.round((game.height - b.sprite.world.y) / BALL_SIZE);
+
+    for (var i = -1; i < 2; i++) {
+        for (var j = -1; j < 2; j++)
+        {
+            if (isInside(brow + i, bcol + j) && Phaser.Math.distance(bx, by, ballPosition[brow + i][bcol + j].sprite.world.x, ballPosition[brow + i][bcol + j].sprite.world.y) < 5 / 2 * BALL_SIZE&&(i!==0||j!==0)) {
+
+                ballPosition[brow + i][bcol + j].sprite.tint = 0x000000;
+                ballPosition[brow + i][bcol + j].sprite.body.dirty = true;
+                selectedBall.push(ballPosition[brow + i][bcol + j]);
+            }
+        }
+    }
+}
+
 function setScore() {
 
     if (selectedBall.length < 3)
     {
-        score = score + 10;
+        if (skill) {
+            score = (score + 10) * 2;
+        }
+        else
+            score = score + 10;
     }
     else
     {
-      score = score + selectedBall.length*5;
+        if (skill) {
+            score = (score + selectedBall.length*5)*2;
+        }
+        else
+            score = score + selectedBall.length * 5;
     }
 }
 
